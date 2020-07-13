5c12a79aeb5114e5ba8d7e5c8ce33ff6c2ee3679
postables <postables@rtradetechnologies.com>
2020-07-12 19:14:02 -0700

    chore: add wip test debugging

--- src/multiaddr/testing.c
+++ src/multiaddr/testing.c
@@ -4,89 +4,91 @@
 #include <stddef.h>
 #include <setjmp.h>
 #include <cmocka.h>
+#include "multiaddr/multiaddr.h"
+#include "multiaddr/varhexutils.h"
 
-#include "test_multiaddr.h"
+#define assert__(x) for ( ; !(x) ; assert(x) )
 
-const char *names[] = {"test_new_from_string",
-                       "test_full",
-                       "test_hex_to_var",
-                       "test_int_to_hex",
-                       "test_multiaddr_utils",
-                       "test_multiaddr_peer_id",
-                       "test_multiaddr_get_peer_id",
-                       "test_multiaddr_bytes",
-                       "test_new_like_libp2p"};
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+void test_multiaddr_peer_id(void **state) {
+    char *orig_address = "QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG";
+    char full_string[255];
+    char *result = NULL;
+    char *bytes = NULL;
+    int retVal = 0, port = 0;
+    struct MultiAddress *addr = NULL, *addr2 = NULL;
+    sprintf(full_string, "/ip4/127.0.0.1/tcp/4001/ipfs/%s/", orig_address);
 
-int (*funcs[])(void) = {test_new_from_string,
-                        test_full,
-                        test_hex_to_var,
-                        test_int_to_hex,
-                        test_multiaddr_utils,
-                        // test_multiaddr_peer_id,
-                        // test_multiaddr_get_peer_id,
-                        test_multiaddr_bytes,
-                        test_new_like_libp2p};
-
-int testit(const char *name, int (*func)(void)) {
-    printf("Testing %s...\n", name);
-    int retVal = func();
-    if (retVal) {
-        printf("%s success!\n", name);
-    } else {
-        printf("** Uh oh! %s failed.**\n", name);
+    addr = multiaddress_new_from_string(full_string);
+    result = multiaddress_get_peer_id(addr);
+    assert__(result) {
+        printf("result is not NULL\n"); 
+    };
+    assert__(strcmp(result, orig_address) == 0) {
+        printf("Original string was %s but new string is %s\n", result, orig_address);
     }
-    return retVal;
+    free(result);
+
+    // switch to bytes and back again to verify the peer id follows...
+
+    // 1. display the original bytes
+    result = Var_To_Hex((char *)addr->bytes, addr->bsize);
+    fprintf(stderr, "Original Bytes: %s\n", result);
+    free(result);
+
+    // make a new MultiAddress from bytes
+    bytes = malloc(addr->bsize);
+    memcpy(bytes, addr->bytes, addr->bsize);
+    // TODO(bonedaddy): this is bugged and doesnt return the correct multiaddress
+    addr2 = multiaddress_new_from_bytes((unsigned char *)bytes, addr->bsize);
+    assert(addr2 != NULL);
+    free(bytes);
+
+    // 2. Display the resultant bytes
+    result = Var_To_Hex((char *)addr2->bytes, addr2->bsize);
+    fprintf(stderr, "New      Bytes: %s\n", result);
+    free(result);
+
+    assert__(strcmp(full_string, addr2->string) == 0) {
+        printf("Original string was %s but new string is %s\n", full_string, addr2->string);
+    }
+
+    port = multiaddress_get_ip_port(addr2);
+    assert__(port == 4001) {
+        printf("Original port was 4001 but new port is %i\n", port);
+    }
+
+    result = multiaddress_get_peer_id(addr2);
+    assert__(strcmp(result, orig_address) == 0) {
+        printf("New peer id %s does not match %s\n", result, orig_address);
+    }
+ 
+    free(result);
 }
 
-int main(int argc, char **argv) {
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+void test_multiaddr_get_peer_id(void **state) {
+    const char *orig_address = "QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG";
+    char full_string[255] = "";
+    char *result = NULL;
+    int retVal = 0;
+    struct MultiAddress *addr = NULL;
+
+    sprintf(full_string, "/ip4/127.0.0.1/tcp/4001/ipfs/%s/", orig_address);
+
+    addr = multiaddress_new_from_string(full_string);
+    result = multiaddress_get_peer_id(addr);
+    assert(result != NULL);
+    int rc = strcmp(orig_address, result);
+    assert(rc == 0);
+    multiaddress_free(addr);
+    free(result);
+}
+
+int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_multiaddr_peer_id),
         cmocka_unit_test(test_multiaddr_get_peer_id),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
-    int counter = 0;
-    int tests_ran = 0;
-    char *test_wanted;
-    int only_one = 0;
-    if (argc > 1) {
-        only_one = 1;
-        if (argv[1][0] == '\'') { // some shells put quotes around arguments
-            argv[1][strlen(argv[1]) - 1] = 0;
-            test_wanted = &(argv[1][1]);
-        } else {
-            test_wanted = argv[1];
-        }
-    }
-    int array_length = sizeof(funcs) / sizeof(funcs[0]);
-    int array2_length = sizeof(names) / sizeof(names[0]);
-    if (array_length != array2_length) {
-        printf("Test arrays are not of the same length. Funcs: %d, "
-               "Names: %d\n",
-               array_length, array2_length);
-    }
-    for (int i = 0; i < array_length; i++) {
-        if (only_one) {
-            const char *currName = names[i];
-            if (strcmp(currName, test_wanted) == 0) {
-                tests_ran++;
-                counter += testit(names[i], funcs[i]);
-            }
-        } else if (!only_one) {
-            tests_ran++;
-            counter += testit(names[i], funcs[i]);
-        }
-    }
-
-    if (tests_ran == 0) {
-        printf("***** No tests found *****\n");
-    } else {
-        if (tests_ran - counter > 0) {
-            printf("***** There were %d failed test(s) (%d "
-                   "successful) *****\n",
-                   tests_ran - counter, counter);
-        } else {
-            printf("All %d tests passed\n", tests_ran);
-        }
-    }
-    return 1;
 }
