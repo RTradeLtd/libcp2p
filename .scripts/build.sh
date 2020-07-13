77516fa1bc01d8a41d3e24d8f7a7ced49d13ebc3
postables <postables@rtradetechnologies.com>
2020-07-13 01:00:19 -0700

    chore: add more flexible build script

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,10 +1,57 @@
 #! /bin/bash
 
+function release_build() {
+    if [[ $(cat build/.last_build) != "release" ]]; then
+        echo "detecting different build mode, recreating build dir"
+        rm -rf build && mkdir build
+    fi
+    echo "building libcp2p in release mode"
+    cd build
+    cmake ..
+    cmake -build .
+    make
+    echo "release" > .last_build
+}
+
+function debug_build() {
+    if [[ $(cat build/.last_build) != "debug" ]]; then
+        echo "detecting different build mode, recreating build dir"
+        rm -rf build && mkdir build
+    fi
+    echo "building libcp2p in debug mode"
+    cd build
+    cmake -D CMAKE_BUILD_TYPE=Debug ..
+    cmake -D CMAKE_BUILD_TYPE=Debug -build . 
+    make
+    echo "debug" > .last_build
+}
+
+function analyze_build() {
+    if [[ $(cat build/.last_build) != "analyze" ]]; then
+        echo "detecting different build mode, recreating build dir"
+        rm -rf build && mkdir build
+    fi
+    echo "building libcp2p in static analysis mode"
+    cd build
+    cmake -D CMAKE_BUILD_TYPE=Analyze ..
+    cmake -D CMAKE_BUILD_TYPE=Analyze -build . 
+    make
+    echo "analyze" > .last_build
+}
+
 if [[ ! -d "build" ]]; then
     mkdir build
 fi
 
-cd build
-cmake ..
-cmake -build .
-make
+
+case "$1" in
+    debug)
+        debug_build
+        ;;
+    analyze)
+        analyze_build
+        ;;
+    *)
+        release_build
+        ;;
+esac
