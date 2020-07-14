1c15ca3bb91b819abf48bbcefbda3f51862d372b
postables <postables@rtradetechnologies.com>
2020-07-14 04:12:46 -0700

    chore: add wip multicodec

---
+++ src/multicodec/multicodec.c
@@ -0,0 +1,9 @@
+#include "multicodec.h"
+
+int multicodec_encode(const char* codecName, char* inData, size_t inDataLength, char* outData, size_t maxOutDataLength) {
+	return 0;
+}
+
+int multicodec_decode(char* inData, size_t inDataLength, char* outData, size_t maxOutDataLength) {
+	return 0;
+}
