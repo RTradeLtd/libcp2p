d34d99f2bd60317369ef37ec2199eba625c5ba71
postables <postables@rtradetechnologies.com>
2020-07-19 20:56:28 -0700

    ci: try another update

--- .scripts/install_cmocka.sh
+++ .scripts/install_cmocka.sh
@@ -1,16 +1,23 @@
 #! /bin/bash
 
 
-# (mkdir cmbuild ; cd cmbuild ; wget https://cmocka.org/files/1.1/cmocka-1.1.5.tar.xz ; tar -Jxvf cmocka-1.1.5.tar.xz ; cd cmocka-1.1.5 ; mkdir build ; cd build ; cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug .. ; make ; sudo make -j$(nproc) install ; sudo ldconfig)
+# (mkdir cmbuild ; cd cmbuild ; wget https://cmocka.org/files/1.1/cmocka-1.1.5.tar.xz ; tar -Jxvf cmocka-1.1.5.tar.xz ; cd cmocka-1.1.5 ; mkdir build ; cd build ; cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug .. ; make ; sudo make -j$(nproc) install ; sudo ldconfig# )
 
-wget https://cmocka.org/files/1.1/cmocka-1.1.5.tar.xz
-tar -Jxvf cmocka-1.1.5.tar.xz 
-cd cmocka-1.1.5
-mkdir build
-cd build
-cmake -D CMAKE_INSTALL_PREFIX=/usr -D CMAKE_BUILD_TYPE=Debug -D CMAKE_C_COMPILER=/usr/bin/gcc-9 .. 
-make
-sudo make -j$(nproc) install
-sudo ldconfig
-cd ../.. # go back to root dir
-rm -rf cmocka-1.1.5  # remove cmocka stuff
+# wget https://cmocka.org/files/1.1/cmocka-1.1.5.tar.xz
+# tar -Jxvf cmocka-1.1.5.tar.xz 
+# cd cmocka-1.1.5
+# mkdir build
+# cd build
+# cmake -D CMAKE_INSTALL_PREFIX=/usr -D CMAKE_BUILD_TYPE=Debug -D CMAKE_C_COMPILER=/usr/bin/gcc-9 .. 
+# make
+# sudo make -j$(nproc) install
+# sudo ldconfig
+# cd ../.. # go back to root dir
+# rm -rf cmocka-1.1.5  # remove cmocka stuff
+
+echo "Building cmocka from source."
+wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
+tar -xJvf cmocka-1.0.1.tar.xz
+cd cmocka-1.0.1 && mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .. && make -j2 && sudo make install
+cd ../..
