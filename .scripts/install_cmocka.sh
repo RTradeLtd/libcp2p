2698f35781ebad148fbd26bb3f97a6f7a7cacad0
postables <postables@rtradetechnologies.com>
2020-07-19 20:32:02 -0700

    make script easier to read

--- .scripts/install_cmocka.sh
+++ .scripts/install_cmocka.sh
@@ -1,4 +1,18 @@
 #! /bin/bash
 
 
-(mkdir cmbuild ; cd cmbuild ; wget https://cmocka.org/files/1.1/cmocka-1.1.5.tar.xz ; tar -Jxvf cmocka-1.1.5.tar.xz ; cd cmocka-1.1.5 ; mkdir build ; cd build ; cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug .. ; make ; sudo make -j$(nproc) install ; sudo ldconfig)
+# (mkdir cmbuild ; cd cmbuild ; wget https://cmocka.org/files/1.1/cmocka-1.1.5.tar.xz ; tar -Jxvf cmocka-1.1.5.tar.xz ; cd cmocka-1.1.5 ; mkdir build ; cd build ; cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug .. ; make ; sudo make -j$(nproc) install ; sudo ldconfig)
+
+mkdir cmbuild
+cd cmbuild
+wget https://cmocka.org/files/1.1/cmocka-1.1.5.tar.xz
+tar -Jxvf cmocka-1.1.5.tar.xz 
+cd cmocka-1.1.5
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug .. 
+make
+sudo make -j$(nproc) install
+sudo ldconfig
+cd "$TRAVIS_BUILD_DIR" # go back to the build directory
+rm -rf cmbuild
