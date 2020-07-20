e78bbc7694f4576668454e1b1ff2d47787b6f1bd
postables <postables@rtradetechnologies.com>
2020-07-20 00:29:07 -0700

    cmake/pkgs: add more intense valgrind memory testing

--- cmake/pkgs/valgrind.cmake
+++ cmake/pkgs/valgrind.cmake
@@ -1,5 +1,5 @@
 macro(define_setup_valgrind)
     find_program(MEMORYCHECK_COMMAND NAMES valgrind)
-    set(MEMORYCHECK_COMMAND_OPTIONS "--tool=massif --leak-check=full --track-origins=yes --tool=memcheck")
+    set(MEMORYCHECK_COMMAND_OPTIONS "--tool=massif --tool=memcheck --leak-check=full --track-origins=yes --show-leak-kinds=all --leak-resolution=high --num-callers=256")
     include(CTest)
 endmacro()
