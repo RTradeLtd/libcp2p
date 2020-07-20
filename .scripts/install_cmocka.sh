4d63ce1d3d19fa591b6fd7f4e375c8b91dea8e7e
postables <postables@rtradetechnologies.com>
2020-07-19 20:52:35 -0700

    chore: cleanup cmocka install script

--- .scripts/install_cmocka.sh
+++ .scripts/install_cmocka.sh
@@ -3,8 +3,6 @@
 
 # (mkdir cmbuild ; cd cmbuild ; wget https://cmocka.org/files/1.1/cmocka-1.1.5.tar.xz ; tar -Jxvf cmocka-1.1.5.tar.xz ; cd cmocka-1.1.5 ; mkdir build ; cd build ; cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug .. ; make ; sudo make -j$(nproc) install ; sudo ldconfig)
 
-mkdir cmbuild
-cd cmbuild
 wget https://cmocka.org/files/1.1/cmocka-1.1.5.tar.xz
 tar -Jxvf cmocka-1.1.5.tar.xz 
 cd cmocka-1.1.5
@@ -14,5 +12,5 @@ cmake -D CMAKE_INSTALL_PREFIX=/usr -D CMAKE_BUILD_TYPE=Debug -D CMAKE_C_COMPILER
 make
 sudo make -j$(nproc) install
 sudo ldconfig
-cd "$TRAVIS_BUILD_DIR" # go back to the build directory
-rm -rf cmbuild
+cd ../.. # go back to root dir
+rm -rf cmocka-1.1.5  # remove cmocka stuff
