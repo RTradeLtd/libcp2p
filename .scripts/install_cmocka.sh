e3d2cf71512d408a506934d6bc2794a86a44fd3a
postables <postables@rtradetechnologies.com>
2020-07-19 20:02:30 -0700

    chore: dont use pushd

--- .scripts/install_cmocka.sh
+++ .scripts/install_cmocka.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 
-(mkdir cmbuild ; cd cmbuild ; wget https://cmocka.org/files/1.1/cmocka-1.1.5.tar.xz ; tar -Jxvf cmocka-1.1.5.tar.xz ; mkdir cmocka-1.1.5/build ; pushd cmocka-1.1.5/build ; cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug .. ; make ; sudo make -j$(nproc) install ; sudo ldconfig)
+(mkdir cmbuild ; cd cmbuild ; wget https://cmocka.org/files/1.1/cmocka-1.1.5.tar.xz ; tar -Jxvf cmocka-1.1.5.tar.xz ; cd cmocka-1.1.5 ; cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug .. ; make ; sudo make -j$(nproc) install ; sudo ldconfig)
