7e11a91f6458cd9886a45a90917f10efddb6602b
postables <postables@rtradetechnologies.com>
2020-07-19 20:35:47 -0700

    chore: specify gcc compiler

--- .scripts/install_cmocka.sh
+++ .scripts/install_cmocka.sh
@@ -10,7 +10,7 @@ tar -Jxvf cmocka-1.1.5.tar.xz
 cd cmocka-1.1.5
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug .. 
+cmake -D CMAKE_INSTALL_PREFIX=/usr -D CMAKE_BUILD_TYPE=Debug -D CMAKE_C_COMPILER=/usr/bin/gcc-9 .. 
 make
 sudo make -j$(nproc) install
 sudo ldconfig
