bdf63eb875dc96e251c4befa4d06d1af2fd39fb9
postables <postables@rtradetechnologies.com>
2020-07-19 19:48:34 -0700

    chore: another attempt at fixing cmocka linking

--- .scripts/install_cmocka.sh
+++ .scripts/install_cmocka.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 
-(mkdir cmbuild ; cd cmbuild ; wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz ; tar -Jxvf cmocka-1.0.1.tar.xz ; mkdir cmocka-1.0.1/build ; pushd cmocka-1.0.1/build ; cmake .. ; cmake -build . ; sudo make -j$(nproc) install ; sudo ldconfig)
+(mkdir cmbuild ; cd cmbuild ; wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz ; tar -Jxvf cmocka-1.0.1.tar.xz ; mkdir cmocka-1.0.1/build ; pushd cmocka-1.0.1/build ; - cmake ../ -DCMAKE_INSTALL_PREFIX=${DESTDIR} -DCMAKE_BUILD_TYPE=Release ; sudo make -j$(nproc) install ; sudo ldconfig)
