8482efb3d2699a5d912ca7edb191e835ebfd3112
postables <postables@rtradetechnologies.com>
2020-07-20 21:55:48 -0700

    chore: build mbedtls and cmocka without unit testing

--- .scripts/install_cmocka.sh
+++ .scripts/install_cmocka.sh
@@ -8,7 +8,7 @@ tar -Jxvf cmocka-1.1.5.tar.xz
 cd cmocka-1.1.5
 mkdir build
 cd build
-cmake -D CMAKE_INSTALL_PREFIX=/usr -D CMAKE_BUILD_TYPE=Debug -D CMAKE_C_COMPILER=/usr/bin/gcc-9 .. 
+cmake -D CMAKE_INSTALL_PREFIX=/usr -D CMAKE_BUILD_TYPE=Debug -D CMAKE_C_COMPILER=/usr/bin/gcc-9 -D UNIT_TESTING=OFF .. 
 make
 sudo make -j$(nproc) install
 sudo ldconfig
--- .scripts/install_mbedtls.sh
+++ .scripts/install_mbedtls.sh
@@ -5,6 +5,7 @@
 git clone https://github.com/ARMmbed/mbedtls.git
 cd mbedtls
 git checkout mbedtls-2.23.0
-make CFLAGS="-fPIC"
+# dont build the tests with mbedtls
+make no_test CFLAGS="-fPIC"
 sudo make install CFLAGS="-fPIC"
 sudo ldconfig
