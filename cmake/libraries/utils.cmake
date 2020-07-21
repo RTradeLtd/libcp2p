47658030a0c268f19e75648d3be174113dfcfc5a
postables <postables@rtradetechnologies.com>
2020-07-21 02:08:15 -0700

    chore: add utils test

--- cmake/libraries/utils.cmake
+++ cmake/libraries/utils.cmake
@@ -10,3 +10,9 @@ add_library(libutils
 
 target_compile_options(libutils PRIVATE ${flags})
 target_link_libraries(libutils pthread)
+
+add_executable(libutils-test ./src/utils/utils_test.c)
+target_compile_options(libutils-test PRIVATE ${flags})
+target_link_libraries(libutils-test libutils)
+target_link_libraries(libutils-test cmocka)
+add_test(NAME LibUtilsTest COMMAND libutils-test)
