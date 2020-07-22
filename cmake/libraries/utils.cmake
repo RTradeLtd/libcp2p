d8c4bc739c3aea72cfa620fd29447b86b659dd07
postables <postables@rtradetechnologies.com>
2020-07-21 17:49:43 -0700

    chore: fix libutils linking with cmocka

--- cmake/libraries/utils.cmake
+++ cmake/libraries/utils.cmake
@@ -11,7 +11,7 @@ add_library(libutils
 target_compile_options(libutils PRIVATE ${flags})
 target_link_libraries(libutils pthread)
 
-add_executable(libutils-test ./src/utils/utils_test.c)
+add_executable(libutils-test ./tests/utils_test.c)
 target_compile_options(libutils-test PRIVATE ${flags})
 target_link_libraries(libutils-test libutils)
 target_link_libraries(libutils-test cmocka)
