cb832970969e63e00f350211fdc5417d62aa76f5
postables <postables@rtradetechnologies.com>
2020-07-22 17:11:43 -0700

    chore: add license headers

--- include/network/socket_server.h
+++ include/network/socket_server.h
@@ -1,3 +1,15 @@
+// Copyright 2020 RTrade Technologies Ltd
+//
+// licensed under GNU AFFERO GENERAL PUBLIC LICENSE;
+// you may not use this file except in compliance with the License;
+// You may obtain the license via the LICENSE file in the repository root;
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /*! @file socket_server.h
  * @brief used to create a tcp/udp socket server listening on multiaddrs
  */
