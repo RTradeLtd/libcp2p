aaf976e126f586afed639319f79cc86aef3b70ec
postables <postables@rtradetechnologies.com>
2020-07-23 21:29:21 -0700

    crypto: remove protobuf functions from key.h

--- include/crypto/key.h
+++ include/crypto/key.h
@@ -7,24 +7,43 @@
  * Utilities for public keys
  */
 
+/*!
+  * @enum key_type_t
+  * @brief denotes the type of key
+  * @details indicates the type of cryptography used with the key
+*/
 typedef enum { KEYTYPE_INVALID, KEYTYPE_ECDSA } key_type_t;
 
+/*!
+  * @struct cbor_encoded_data
+  * @typedef cbor_encoded_data_t
+  * @brief wraps data returned from encoding with tinycbor
+*/
 typedef struct cbor_encoded_data {
-    uint8_t *data;
-    size_t len;
+    uint8_t *data; /*! @brief the actual cbor encoded data */
+    size_t len; /*! @brief the length of the cbor encoded data */
 } cbor_encoded_data_t;
 
+/*! @struct public_key
+  * @typedef public_key_t
+  * @brief a structure containing information needed for using a public key
+  * @note to send over the wire use libp2p_crypto_public_key_cbor_encode
+*/
 typedef struct public_key {
-    key_type_t type;
-    unsigned char *data;
-    size_t data_size;
+    key_type_t type; /*! @brief the type of the key */
+    unsigned char *data; /*! @brief the key data in PEM format */
+    size_t data_size; /*! @brief the size of the key data */
 } public_key_t;
 
+/*! @struct private_key
+  * @typedef private_key_t
+  * @brief  a structure containing information needed for using a private key
+*/
 typedef struct private_key {
-    key_type_t type;
-    unsigned char *data;
+    key_type_t type; /*! @brief the type of the key */
+    unsigned char *data; /*! @brief the key data in PEM format */
     size_t data_size;
-} private_key_t;
+} private_key_t; /*! @brief the size of the key data */
 
 public_key_t *libp2p_crypto_public_key_new(void);
 void libp2p_crypto_public_key_free(public_key_t *in);
@@ -35,45 +54,19 @@ int libp2p_crypto_private_key_copy(const private_key_t *source,
                                    private_key_t *destination);
 
 /*!
- * @brief used to cbor decode a uint8_t pointer and return a public_key_t object
+ * @brief used to cbor data a chunk of data
+ * @param data an instance of cbor_encoded_data_t created with  libp2p_crypto_public_key_cbor_encode 
+ * @returns Success: pointer to a public_key_t instance
+ * @returns Failure: NULL pointer
  */
 public_key_t *libp2p_crypto_public_key_cbor_decode(cbor_encoded_data_t *data);
 
 /*!
- * @brief used to cbor encode a public_key_t object
+ * @brief used to cbor encode a public_key_t type for sending over the wire
+ * @param pub_key an instance of public_key_t fully filled out
+ * @param bytes_written returns the number of bytes written
+ * @return Success: pointer to an instance of cbor_encoded_data_t
+ * @return Failure: NULL pointer
  */
 cbor_encoded_data_t *libp2p_crypto_public_key_cbor_encode(public_key_t *pub_key,
                                                           size_t *bytes_written);
-
-/**
- * @brief Unmarshal a public key from a protobuf
- */
-int libp2p_crypto_public_key_protobuf_decode(unsigned char *buffer,
-                                             size_t buffer_length,
-                                             public_key_t **out);
-
-size_t libp2p_crypto_public_key_protobuf_encode_size(const public_key_t *in);
-
-int libp2p_crypto_public_key_protobuf_encode(const public_key_t *in,
-                                             unsigned char *buffer,
-                                             size_t max_buffer_length,
-                                             size_t *bytes_written);
-// private key
-int libp2p_crypto_private_key_protobuf_decode(unsigned char *buffer,
-                                              size_t buffer_length,
-                                              private_key_t **out);
-
-size_t libp2p_crypto_private_key_protobuf_encode_size(const private_key_t *in);
-
-int libp2p_crypto_private_key_protobuf_encode(const private_key_t *in,
-                                              unsigned char *buffer,
-                                              size_t max_buffer_length,
-                                              size_t *bytes_written);
-
-/**
- * @brief convert a public key into a peer id
- * @param public_key the public key struct
- * @param peer_id the results, in a null-terminated string
- * @returns true(1) on success, otherwise false(0)
- */
-int libp2p_crypto_public_key_to_peer_id(public_key_t *public_key, char *peer_id);
