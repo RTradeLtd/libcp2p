0107a7af4c4d3df60b4646f066b42213592bec5f
postables <postables@rtradetechnologies.com>
2020-07-25 21:24:06 -0700

    chore: add CLI framework

--- src/cli/command_line.c
+++ src/cli/command_line.c
@@ -1,98 +1,94 @@
 
 /*! @file command_line.c
- * @brief provides a basic CLI building tool
- * attribution note: modified version of commander see `deps/commander` for the
- * license for that package uses argtable3 for command line configuration and allows
- * loading a list of commands to execute using the `--conmmand` or `-c` CLI flag you
- * can specify the command out of the list of commands that have been loaded before
- * calling execute you'll want to load the appropriate `argc` and `argv` values for
- * the callback of the command you want to make
- * @note from https://github.com/bonedaddy/c-template
- */
+  * @brief provides a basic CLI building tool
+  * attribution note: modified version of commander see `deps/commander` for the license for that package
+  * uses argtable3 for command line configuration and allows loading a list of commands to execute
+  * using the `--conmmand` or `-c` CLI flag you can specify the command out of the list of commands that have been loaded
+  * before calling execute you'll want to load the appropriate `argc` and `argv` values for the callback of the command you want to make
+  * @note from https://github.com/bonedaddy/c-template
+*/
 
-#include "cli/command_line.h"
-#include "thirdparty/argtable3/argtable3.h"
-#include "utils/colors.h"
-#include "utils/logger.h"
-#include <stdbool.h>
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <argtable3.h>
+#include "utils/colors.h"
+#include "cli/command_line.h"
+#include "utils/logger.h"
 
 /*! @brief intializes a new command_object to have commands loaded into
- */
+*/
 command_object *new_command_object(int argc, char *argv[]) {
-    // if too many arguments have been provided return a null pointer
-    if (argc > MAX_COMMAND_ARGS) {
-        print_colored(COLORS_RED, "too many command line arguments provided\n");
-        return NULL;
+  // if too many arguments have been provided return a null pointer
+  if (argc > MAX_COMMAND_ARGS) {
+    print_colored(COLORS_RED, "too many command line arguments provided\n");
+    return NULL;
+  }
+  // allocate memory equal to the size of command_object  combined with the size of all provided arguments
+  // and typecast it to command_object *
+  // command_object *pcobj = (command_object *)malloc(sizeof(command_object) + sizeof(*argv));
+  // this prevents valgrind from reporting an error
+  command_object *pcobj = calloc(sizeof(command_object), sizeof(*argv));
+  if (pcobj == NULL) {
+    printf("failed to malloc command_object\n");
+    return NULL;
+  }
+  // set arg count
+  pcobj->argc = argc;
+  // set command_count
+  pcobj->command_count = 0;
+  // parse cli arguments provided by user  and assign to pcobj
+  for (int i = 0; i < argc; i++) {
+    pcobj->argv[i] = malloc(strlen(argv[i]) + 1);
+    if (pcobj->argv[i] == NULL) {
+      printf("failed to malloc argv space\n");
+      return NULL;
     }
-    // allocate memory equal to the size of command_object  combined with the size of
-    // all provided arguments and typecast it to command_object * command_object
-    // *pcobj = (command_object *)malloc(sizeof(command_object) + sizeof(*argv));
-    // this prevents valgrind from reporting an error
-    command_object *pcobj = calloc(1, sizeof(command_object));
-    if (pcobj == NULL) {
-        printf("failed to malloc command_object\n");
-        return NULL;
-    }
-    // set arg count
-    pcobj->argc = argc;
-    // set command_count
-    pcobj->command_count = 0;
-    // parse cli arguments provided by user  and assign to pcobj
-    for (int i = 0; i < argc; i++) {
-        pcobj->argv[i] = calloc(1, strlen(argv[i]) + 1);
-        if (pcobj->argv[i] == NULL) {
-            printf("failed to malloc argv space\n");
-            return NULL;
-        }
-        strcpy(pcobj->argv[i], argv[i]);
-    }
-    return pcobj;
+    strcpy(pcobj->argv[i], argv[i]);
+  }
+  return pcobj;
 }
 
 /*! @brief loads command handler and makes it executable
- */
+*/
 int load_command(command_object *self, command_handler *command) {
-    if (self == NULL) {
-        return -1;
-    }
-    if (self->command_count >= MAX_COMMANDS) {
-        print_colored(COLORS_RED, "maximum number of commands\n");
-        return -1;
-    }
-    int n = self->command_count++;
-    self->commands[n] = command;
-    return 0;
+  if (self == NULL) {
+    return -1;
+  }
+  if (self->command_count >= MAX_COMMANDS) {
+    print_colored(COLORS_RED, "maximum number of commands\n");
+    return -1;
+  }
+  int n = self->command_count++;
+  self->commands[n] = command;
+  return 0;
 }
 
 /*! @brief checks to see if we have a command named according to run and executes it
- */
+*/
 int execute(command_object *self, char *run) {
-    for (int i = 0; i < self->command_count; i++) {
-        if (strcmp(self->commands[i]->name, run) == 0) {
-            self->commands[i]->callback(self->argc, self->argv);
-            print_colored(COLORS_GREEN, "[info] command executed successfully\n");
-            return 0;
-        }
+  for (int i = 0; i < self->command_count; i++) {
+    if (strcmp(self->commands[i]->name, run) == 0) {
+      self->commands[i]->callback(self->argc, self->argv);
+      print_colored(COLORS_GREEN, "[info] command executed successfully\n");
+      return 0;
     }
-    print_colored(COLORS_RED, "[error] failed to execute command\n");
-    return -1;
+  }
+  print_colored(COLORS_RED, "[error] failed to execute command\n");
+  return -1;
 }
 
 /*! @brief frees memory allocated for the command_object and sets pointer to null
- * for some reason this is causing an address boundary error
- */
+   * for some reason this is causing an address boundary error 
+*/
 void free_command_object(command_object *self) {
-    for (int i = 0; i < self->command_count; i++) {
-        free(self->commands[i]);
-    }
-    for (int i = 0; i < self->argc; i++) {
-        free(self->argv[i]);
-    }
-    free(self);
-}
+  for (int i = 0; i < self->command_count; i++) {
+      free(self->commands[i]);
+  }
+  free(self);
+} 
 
 // validates argtable and parses command line arguments
 int parse_args(int argc, char *argv[], void *argtable[]) {
@@ -122,19 +118,18 @@ void print_help(char *program_name, void *argtable[]) {
 void setup_args(const char *version_string) {
     help = arg_litn(NULL, "help", 0, 1, "displays help menu");
     version = arg_litn(NULL, "version", 0, 1, version_string);
-    file = arg_filen(NULL, "file", "<file>", 0, 10,
-                     "file(s) to load data from max of 10");
+    file = arg_filen(NULL, "file", "<file>", 0, 10, "file(s) to load data from max of 10");
     output = arg_filen(NULL, "output", "<file>", 0, 1, "file to output data too");
     command_to_run = arg_strn("c", "command", "<command>", 0, 1, "command to run");
     end = arg_end(20);
-}
+} 
 
 char *get_run_command() {
-    char *run_command = malloc(strlen(*command_to_run->sval));
-    if (run_command == NULL) {
-        printf("failed to malloc run_command\n");
-        return NULL;
-    }
-    strcpy(run_command, (char *)*command_to_run->sval);
-    return run_command;
+  char *run_command = malloc(strlen(*command_to_run->sval));
+  if (run_command == NULL) {
+    printf("failed to malloc run_command\n");
+    return NULL;
+  }
+  strcpy(run_command, (char *)*command_to_run->sval);
+  return run_command;
 }
