85ff7d4c1c326205756541d94a03259a16f51ca4
postables <postables@rtradetechnologies.com>
2020-07-25 23:47:09 -0700

    src/network: dont close socket when handling rpc fails

--- src/network/socket_server.c
+++ src/network/socket_server.c
@@ -454,7 +454,6 @@ void handle_inbound_rpc(void *data) {
         case 0:
             hdata->srv->thl->log(hdata->srv->thl, 0, "client disconnected",
                                  LOG_LEVELS_DEBUG);
-            close(hdata->conn->socket_number);
             free(hdata->conn);
             free(hdata);
             return;
@@ -462,7 +461,6 @@ void handle_inbound_rpc(void *data) {
             hdata->srv->thl->logf(hdata->srv->thl, 0, LOG_LEVELS_ERROR,
                                   "error encountered during read %s",
                                   strerror(errno));
-            close(hdata->conn->socket_number);
             free(hdata->conn);
             free(hdata);
             return;
