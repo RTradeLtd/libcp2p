34f39c1e1e4ad4c17eb92d745a296addc27100a3
postables <postables@rtradetechnologies.com>
2020-07-25 19:13:53 -0700

    network: add wip message encoding/decoding

--- src/network/messages.c
+++ src/network/messages.c
@@ -1,32 +1,25 @@
 /*! @file messages.c
- * @brief defines message types and tooling for a very minimal RPC framework
- * @note when sending messages anytime you send a new message, you must first send a
- * single byte that indicates the size of the messsage we are sending
- * @note this helps ensure that we can appropriately handle new requests and allocate
- * enough memory
- * @details the message types here are intended to be served as a building block for
- * your own applications
- * @details at a minimum the types here are setup to establish a secure
- * communications channel using ECDSA keys and ECDH key agreement
- */
+  * @brief defines message types and tooling for a very minimal RPC framework
+  * @note when sending messages anytime you send a new message, you must first send a single byte that indicates the size of the messsage we are sending
+  * @note this helps ensure that we can appropriately handle new requests and allocate enough memory
+  * @details the message types here are intended to be served as a building block for your own applications
+  * @details at a minimum the types here are setup to establish a secure communications channel using ECDSA keys and ECDH key agreement
+*/
 
-#include "network/socket.h"
 #include "network/messages.h"
 #include "encoding/cbor.h"
 #include "tinycbor/cbor.h"
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <netdb.h>
+#include <stdio.h>
 
 /*!
- * @brief used to cbor encode a message_t instance
- * @param msg pointer to an instance of message_t
- * @return Success: pointer to an instance of cbor_encoded_data_t
- * @return Failure: NULL pointer
- */
-cbor_encoded_data_t *cbor_encode_message_t(message_t *msg) {
+  * @brief used to cbor encode a message_t instance
+  * @param msg pointer to an instance of message_t
+  * @return Success: pointer to an instance of cbor_encoded_data_t
+  * @return Failure: NULL pointer
+*/
+cbor_encoded_data_t *cbor_encoded_message_t(message_t *msg) {
     uint8_t buf[sizeof(message_t) + msg->len];
     CborEncoder encoder, array_encoder;
     CborError err;
@@ -69,17 +62,17 @@ cbor_encoded_data_t *cbor_encode_message_t(message_t *msg) {
     if (cbdata == NULL) {
         return NULL;
     }
-    return cbdata;
+    return cbdata;       
 }
 
 /*!
- * @brief used to cbor decode data into a message_t instance
- * @param input an instance of cbor_encoded_data_t containing the data to decode
- * @return Success: poitner to an instance of message_t
- * @return Failure: NULL pointer
- */
+  * @brief used to cbor decode data into a message_t instance
+  * @param input an instance of cbor_encoded_data_t containing the data to decode
+  * @return Success: poitner to an instance of message_t
+  * @return Failure: NULL pointer
+*/
 message_t *cbor_decode_message_t(cbor_encoded_data_t *input) {
-    CborParser parser;
+    CborParser parser; 
     CborValue value, recurse;
     CborError err;
 
@@ -164,12 +157,12 @@ message_t *cbor_decode_message_t(cbor_encoded_data_t *input) {
         printf("failed to leave container: %s\n", cbor_error_string(err));
         return NULL;
     }
-
-    message_t *msg = calloc(1, sizeof(message_t));
+    
+    message_t *msg = calloc(sizeof(message_t), sizeof(message_t));
     if (msg == NULL) {
         return NULL;
     }
-    msg->data = calloc(1, data_len);
+    msg->data = calloc(sizeof(unsigned char), data_len);
     if (msg->data == NULL) {
         free(msg);
         return NULL;
@@ -178,164 +171,4 @@ message_t *cbor_decode_message_t(cbor_encoded_data_t *input) {
     msg->type = (MESSAGE_TYPES_T)message_type;
     msg->len = data_len;
     return msg;
-}
-
-/*!
- * @brief frees up resources allocated for an instance of message_t
- * @param msg an instance of message_t
- */
-void free_message_t(message_t *msg) {
-    free(msg->data);
-    free(msg);
-}
-
-/*!
- * @brief returns the size of a message_t instance
- */
-size_t size_of_message_t(message_t *msg) {
-    if (msg == NULL) {
-        return 0;
-    }
-
-    size_t size = 0;
-    size += sizeof(*msg);
-    size += msg->len;
-
-    return size;
-}
-
-
-/*!
-  * @brief used to handle receiving data from a UDP or TCP socket
-  * @details it is designed to reduce the manual overhead with regards to processing messages
-  * @details because the first byte of any data stream coming in defines the size of the total data to receive
-  * @details and the remaining data defines the actual cbor encoded data. therefore we need to properly parse this information
-  * @details and the manner of processing is useful to either the server or client side of things
-  * @param thl an instance of a thread_logger, can be NULL to disable logging
-  * @param socket_num the file descriptor of the socket to receive from
-  * @param is_tcp indicates whether this is a TCP socket
-  * @param max_buffer_len specifies the maximum buffer length we are willing to allocate memory for
-  * @return Success: pointer to an a chunk of memory containing an instance of cbor_encoded_data_t
-  * @return Failure: NULL pointer
-  * @warning we will allocate slightly more memory than max_buffer_len since this refers to the maximum buffer size of the data member of a cbor_encoded_data_t instance, althoug h it will only be a few bytes more
-*/
-message_t *handle_receive(thread_logger *thl, int socket_number, bool is_tcp, size_t max_buffer_len) {
-
-    size_t rc = 0;
-    bool failed = false;
-    char first_byte[1];
-    memset(first_byte, 0, 1);
-
-    if (is_tcp == true) {
-        rc = recv(socket_number, first_byte, 1, 0);
-    } else {
-        rc = recvfrom(socket_number, first_byte, 1, 0, NULL, NULL);
-    }
-
-    failed = recv_or_send_failed(thl, rc);
-    if (failed == true) {
-        return NULL;
-    }
-
-    /*!
-     * @brief first attempt to derive the size by using atoi
-     * @brief occasionally this will fail and give 0, even though
-     * @brief there is a non-zero value there
-     * @brief in cases where it fails casting to int returns the correct value
-     * @warning in certain cases casting to int returns the wrong value too
-     */
-    int message_size = atoi(first_byte);
-    if (message_size == 0) {
-        message_size = (int)first_byte[0];
-        if (message_size == 0) {
-            return NULL;
-        }
-    }
-
-    /*!
-      * @brief abort further handling if message size is less than or equal to 0
-      * @brief greater than the max RPC message size OR greater than the buffer
-    */
-    if (message_size <= 0 || message_size >= MAX_RPC_MSG_SIZE_KB || message_size > (int)max_buffer_len) {
-        if (thl != NULL) {
-            thl->log(thl, 0, "invalid message size", LOG_LEVELS_DEBUG);
-        }
-        return NULL;
-    }
-
-    unsigned char buffer[max_buffer_len];
-    memset(buffer, 0, max_buffer_len);
-
-    if (is_tcp == true) {
-        rc = recv(socket_number, buffer, max_buffer_len, 0);
-    } else {
-        rc = recvfrom(socket_number, buffer, max_buffer_len, 0, NULL, NULL);
-    }
-
-    failed = recv_or_send_failed(thl, rc);
-    if (failed == true) {
-        return NULL;
-    }
-    
-    // dont allocate memory for this struct so we can use stack memory
-    cbor_encoded_data_t cbdata = {.len = rc, .data = buffer};
-
-    return cbor_decode_message_t(&cbdata);
-}
-
-/*!
-  * @brief used to handle sending data through a TCP or UDP socket
-  * @details designed to reduce manual overhead with sending RPC messages
-  * @details it takes care of encoding the given message_t object into a CBOR object
-  * @details and then sending the CBOR object through the wire
-  * @return Success: 0
-  * @return Failure: -1
-*/
-int handle_send(thread_logger *thl, int socket_number, bool is_tcp, message_t *msg, addr_info *peer_address) {
-
-    if (is_tcp == false && peer_address == NULL) {
-        return -1;
-    }
-
-    cbor_encoded_data_t *cbdata = cbor_encode_message_t(msg);
-    if (cbdata == NULL) {
-        return -1;
-    }
-
-    size_t cbor_len = get_encoded_send_buffer_len(cbdata);
-    if (cbor_len <= 0) {
-        free_cbor_encoded_data(cbdata);
-        return -1;
-    }
-
-    unsigned char send_buffer[cbor_len];
-    memset(send_buffer, 0, cbor_len);
-
-    int rc = get_encoded_send_buffer(cbdata, send_buffer, cbor_len);
-
-    // free memory as the cbor encoded data struct is no longer needed
-    free_cbor_encoded_data(cbdata);
-
-    if (rc == -1) {
-        return -1;
-    }
-
-    if (is_tcp == true) {
-        rc = send(socket_number, send_buffer, sizeof(send_buffer), 0);
-    } else {
-        /*! 
-          * @todo we likely need to refactor this and send two datagrams
-          * @todo the first datagram containing the messsage size and
-          * @todo the second datagram containing the actual message data
-        */
-        rc = sendto(socket_number, send_buffer, sizeof(send_buffer), 0, peer_address->ai_addr, peer_address->ai_addrlen);
-    }
-
-    bool failed = recv_or_send_failed(thl, rc);
-    if (failed == true) {
-        return -1;
-    }
-
-    return 0;
-
 }
