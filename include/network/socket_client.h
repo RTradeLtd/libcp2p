0475d2927b5fe79343e13b3925b971380ba9c47c
postables <postables@rtradetechnologies.com>
2020-07-28 22:18:36 -0700

    network: have client send_to helper use unsigned char

--- include/network/socket_client.h
+++ include/network/socket_client.h
@@ -46,9 +46,10 @@ socket_client_t *new_socket_client(thread_logger *thl, multi_addr_t *addr);
  * @brief used to send a message through the connected socket number
  * @param client an instance of socket_client_t created with new_socket_client
  * @param peer_address the target address to connect to through the socket
- * @param message a null terminated pointer to a char
+ * @param message the actual message data to send
+ * @param message_len the length of the message data
  * @returns Success: 0
  * @returns Failure: 1
  */
 int socket_client_sendto(socket_client_t *client, addr_info *peer_address,
-                         char *message);
+                         unsigned char *message, size_t message_len);
