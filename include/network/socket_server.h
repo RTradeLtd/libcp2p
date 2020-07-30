764587b5dfaeea57ffe7d1bc2f865ff541b39057
postables <postables@rtradetechnologies.com>
2020-07-30 00:07:37 -0700

    network: add wip function to enable easier sending to target hosts

--- include/network/socket_server.h
+++ include/network/socket_server.h
@@ -188,3 +188,21 @@ void setup_signal_shutdown(int signals[], int num_signals);
  * @brief used to negotiate a secure connection with the current connection
  */
 bool negotiate_secure_connection(conn_handle_data_t *data);
+
+
+/*!
+  * @brief uses existing server sockets to send data to other socket servers
+  * @details uses existing sockets to send a message to the given multiaddress
+  * @details if the multiaddress is for the UDP protocol and we have no available UDP file descriptors we return an error
+  * @details additionally if the multiaddress is for the TCP protocol and we have no aavailable TCP file descriptors we retun an error
+  * @param srv the server whoses sockets we'll leverage
+  * @param to_address the multiaddress of the host to send data to
+  * @param buffer the actual data to send
+  * @param buffer_len the size of the buffer
+  * @todo support better source file descriptor selection as this simply uses the first available socket
+  * @todo in cases where we only have 1  socket for each protocol (TCP & UDP) this is fine
+  * @todo but in the even of having multiple sockets for each protocol this could become problematic
+  * @return Success: 0
+  * @return Failure: -1
+*/
+int socket_server_sendto(socket_server_t *srv, multi_addr_t *to_address, unsigned char *buffer, size_t buffer_len);
