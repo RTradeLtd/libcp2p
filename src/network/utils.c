1c04c7f9d788e83f0e638400b97b21ab08ac66d3
postables <postables@rtradetechnologies.com>
2020-07-31 18:13:02 -0700

    network: properly receive and send message size

--- src/network/utils.c
+++ src/network/utils.c
@@ -4,6 +4,9 @@
 #include <stdint.h>
 #include <unistd.h>
 
+
+// from https://stackoverflow.com/questions/9140409/transfer-integer-over-a-socket-in-c/9142150#9142150
+
 /*!
  * @brief utility function to send an int through a socket
  * @details handles situations where we may send less than expected bytes
